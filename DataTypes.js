6d70d386d92b3f6962cb8561c43204ad68235d70
Szymon Bajbak <sereksweterek@gmail.com>
2021-10-27 23:25:01 +0200

    Commit without message

---
+++ DataTypes.js
@@ -0,0 +1,31 @@
+/* Data Types:
+undefined
+null
+boolean
+string
+symbol
+number
+object
+*/
+
+//The are 3 ways to set a variable in javascript
+var myName = "Szymon"; //can be used in the entire program
+
+let ourName = "freeCodeCamp" //can be used only in the scope
+
+const pi =3.14 //can't ever be changed
+
+myName = 8
+
+var a; //you can skip semi-colon in java script if it is followed by a line break, but it's recommended for clarity
+
+var b =2;
+console.log(a)
+
+a =7;
+
+b=a
+
+console.log(a)
+
+var a = 9;
---
+++ index.js
@@ -0,0 +1,11 @@
+var number = 5; //in-line comment
+
+/* this is a
+ multi-line 
+ comment
+fddfdf
+dfdf
+dfd
+dfd
+
+*/
